3149992e9d997ea7221d28a8be053702f2260274
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-04-14 13:33:30 +0100

    Add basic repo test

---
+++ src/services/genres-repo.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, beforeEach } from 'node:test';
+import assert from 'node:assert/strict';
+
+describe('GenresRepo', () => {
+    beforeEach(() => {
+        // Setup code before each test
+    });
+
+    it('Should real all genres', async () => {
+        // Test readAll
+    });
+
+    it('Should real a genre by id', async () => {
+        // Test readById
+    });
+});
