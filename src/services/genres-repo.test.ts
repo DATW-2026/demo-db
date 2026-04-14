af26a32b0d82a09788b0871326d867f57f9c43c4
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-04-14 14:00:35 +0100

    Implement genres repo tests

--- src/services/genres-repo.test.ts
+++ src/services/genres-repo.test.ts
@@ -1,16 +1,34 @@
 import { describe, it, beforeEach } from 'node:test';
 import assert from 'node:assert/strict';
 
-describe('GenresRepo', () => {
+import { GenresRepo } from './genres-repo.ts';
+import { connectDB } from '../config/db.ts';
+
+describe('GenresRepo', async () => {
+    const pool = await connectDB();
+    const genresRepo = new GenresRepo(pool);
     beforeEach(() => {
-        // Setup code before each test
+        //
     });
 
     it('Should real all genres', async () => {
-        // Test readAll
+        const genres = await genresRepo.readAllGenres();
+        assert(Array.isArray(genres));
+        assert.strictEqual(genres.length, 12);
     });
 
     it('Should real a genre by id', async () => {
-        // Test readById
+        const genre = await genresRepo.readGenreById(1);
+        assert(genre);
+        assert.strictEqual(genre.id, 1);
+    });
+
+    it('Should throw error if genre not found', async () => {
+        try {
+            await genresRepo.readGenreById(999);
+            assert.fail('Expected an error to be thrown');
+        } catch (error) {
+            assert.strictEqual(error, error as Error);
+        }
     });
 });
