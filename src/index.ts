f49108d49b3be0498c5051d9e79389b062d3b34f
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-04-14 12:48:44 +0100

    Update select sample with parametrice query

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import debug from 'debug';
 
 import { env } from './config/env.ts';
 import { connectDB } from './config/db.ts';
+import type { Genre } from './schemas/genre.ts';
 
 const log = debug(`${env.PROJECT_NAME}:index`);
 log('Starting application');
 
 const pool = await connectDB();
-const { rows } = await pool.query('SELECT * FROM genres;');
-log(rows);
+const id = 1;
+const { rows } = await pool.query<Genre>(
+    'SELECT genre_id as id, name FROM genres WHERE genre_id = $1',
+    [id],
+);
+log(rows[0]);
