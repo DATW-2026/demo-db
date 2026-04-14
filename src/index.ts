00d5a6e495e5c66239cdda5f0af29ccb1e04ef6a
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-04-14 13:32:49 +0100

    Add test to genre repo

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,20 @@ import debug from 'debug';
 
 import { env } from './config/env.ts';
 import { connectDB } from './config/db.ts';
-import type { Genre } from './schemas/genre.ts';
+import { GenresRepo } from './services/genres-repo.ts';
 
 const log = debug(`${env.PROJECT_NAME}:index`);
 log('Starting application');
 
 const pool = await connectDB();
-const id = 1;
-const { rows } = await pool.query<Genre>(
-    'SELECT genre_id as id, name FROM genres WHERE genre_id = $1',
-    [id],
-);
-log(rows[0]);
+const genresRepo = new GenresRepo(pool);
+
+const g = await genresRepo.readAllGenres();
+log('Genres:', g);
+
+try {
+    const g2 = await genresRepo.readGenreById(1);
+    log(`Genres with id 1:`, g2);
+} catch (error) {
+    log((error as Error).message);
+}
