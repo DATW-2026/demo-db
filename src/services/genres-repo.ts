cacd27cf72b66aaad501e830f5f2d51c5cc72d29
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-04-15 10:49:49 +0100

    Update genre repo with read, create, update and delete queries

--- src/services/genres-repo.ts
+++ src/services/genres-repo.ts
@@ -32,10 +32,63 @@ export class GenresRepo {
         if (rows.length === 0) {
             throw new SqlError(`Genre with id ${id} not found`, {
                 code: 'NOT_FOUND',
+                sqlState: 'READ_FAILED',
                 sqlMessage: `No genre found with id ${id}`,
             });
         }
 
         return rows[0];
+    }
+
+    async createGenre(name: string) {
+        const q = `
+            INSERT INTO genres (name)
+            VALUES ($1)
+            RETURNING genre_id AS id, name
+        `;
+        const { rows } = await this.#pool.query<Genre>(q, [name]);
+
+        return rows[0];
+    }
+
+    async updateGenre(id: number, name: string) {
+        const q = `
+        UPDATE genres
+            SET name = $2
+            WHERE genre_id = $1
+            RETURNING genre_id AS id, name
+        `;
+
+        const { rows } = await this.#pool.query<Genre>(q, [id, name]);
+
+        if (rows.length === 0) {
+            throw new SqlError(`Genre with id ${id} not found`, {
+                code: 'NOT_FOUND',
+                sqlState: 'UPDATE_FAILED',
+                sqlMessage: `No genre found with id ${id}`,
+            });
+        }
+
+        return rows[0] as Genre;
+    }
+
+    async deleteGenre(id: number) {
+        const q = `
+        DELETE FROM genres
+            WHERE genre_id = $1
+            RETURNING genre_id as id, name
+        `;
+
+        const { rows } = await this.#pool.query<Genre>(q, [id]);
+
+        if (rows.length === 0) {
+            throw new SqlError(`Genre with id ${id} not found`, {
+                code: 'NOT_FOUND',
+                sqlState: 'DELETE_FAILED',
+                sqlMessage: `No genre found with id ${id}`,
+            });
+        }
+
+        return rows[0] as Genre;
     }
 }
