6fe15538167219b9c0dac7280f96317b8683cc28
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-04-15 13:38:17 +0100

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import debug from 'debug';
 
 import { env } from './config/env.ts';
+import { MoviesRepo } from './services/movies-repo.ts';
 import { connectDB } from './config/db.ts';
-import { GenresRepo } from './services/genres-repo.ts';
+//import { GenresRepo } from './services/genres-repo.ts';
 
 const log = debug(`${env.PROJECT_NAME}:index`);
 log('Starting application');
 
 const pool = await connectDB();
-const genresRepo = new GenresRepo(pool);
+// const genresRepo = new GenresRepo(pool);
+const moviesRepo = new MoviesRepo(pool);
 
-const g = await genresRepo.readAllGenres();
-log('Genres:', g);
+// const g = await genresRepo.readAllGenres();
+// log('Genres:', g);
 
-try {
-    const g2 = await genresRepo.readGenreById(1);
-    log(`Genres with id 1:`, g2);
-} catch (error) {
-    log((error as Error).message);
-}
+// try {
+//     const g2 = await genresRepo.readGenreById(1);
+//     log(`Genres with id 1:`, g2);
+// } catch (error) {
+//     log((error as Error).message);
+// }
+
+const movies = await moviesRepo.readAllMoviesWithGenres();
+log(movies);
