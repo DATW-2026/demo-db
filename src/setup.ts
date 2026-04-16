c3de0d8f98d888007a1ccf38c32139e34cdfcf51
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-04-16 10:43:09 +0100

    Add setup db file and npm command

---
+++ src/setup.ts
@@ -0,0 +1,8 @@
+import { connectDB } from './config/db.ts';
+import { prepareTestingDB } from './config/prepare-testing-db.ts';
+
+const db = await connectDB();
+await prepareTestingDB(db);
+
+console.log('Testing DB setup completed successfully');
+process.exit(0);
